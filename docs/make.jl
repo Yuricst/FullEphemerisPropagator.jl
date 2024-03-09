ce6963cf333f3a15e1da11b7344ad9027d46ed85
Yuri <yuri.shimane@gmail.com>
2024-03-09 03:11:57 -0500

    rebuilt

--- docs/make.jl
+++ docs/make.jl
@@ -16,22 +16,9 @@ makedocs(
     # options
     pages = [
 		"Home" => "index.md",
-        "Basics" => "basic.md",
-		# "Basics" => Any[
-		# 	"Basic Example" => "tutorial.md",
-		# 	"Optimization Problem Options" => "optimization_problem.md",
-		# 	"Export & Import Solution" => "handle_io.md",
-		# 	"Thruster Models" => "thruster.md",
-		# ],
-		# "Advanced Configurations" => Any[
-		# 	"Variable number of nodes" => "advanced_config/variable_nsegs.md",
-		# 	"Match-point Definitions" => "advanced_config/matchpoint.md",
-		# ],
-		# "Advanced Examples" => Any[
-		# 	"Mission to asteroids" => "asteroid_missions.md",
-		# 	"Global Optimization with MBH" => "use_mbh.md",
-		# 	"Arrival to Manifold" => "manifold_arrival.md",
-		# ],
+        "Tutorials" => Any[
+            "Basics" => "basics.md",
+        ],
 		"API" => Any[
 			"Core" => "api/api_core.md",
 			# "Problem Constructor" => "api/api_create_sft_problem.md",
--- docs/src/basics.md
+++ docs/src/basics.md
@@ -1,4 +1,4 @@
-# Basic example
+# Basics
 
 For the N-body problem, we can first do some setup:
 
