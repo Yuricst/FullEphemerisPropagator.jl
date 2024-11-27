e3f53af3d4b13636e6edca7772ce2de59f4ad7a3
Yuri <yuri.shimane@gmail.com>
2024-11-27 01:26:06 -0500

    test dir init

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,1 +1,20 @@
 """Run tests"""
+
+using GLMakie
+using LinearAlgebra
+using OrdinaryDiffEq
+using SPICE
+using Test
+
+include(joinpath(@__DIR__, "../src/FullEphemerisPropagator.jl"))
+
+# furnish spice kernels
+spice_dir = ENV["SPICE"]
+furnsh(joinpath(spice_dir, "lsk", "naif0012.tls"))
+furnsh(joinpath(spice_dir, "spk", "de440.bsp"))
+
+
+
+@time @testset "Propagator object tests" begin
+    include("test_stm.jl")
+end
