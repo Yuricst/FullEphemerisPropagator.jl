cdd5e580387598c523d7ceed7c2899de167a0d8c
Yuri <yuri.shimane@gmail.com>
2024-11-27 12:30:54 -0500

    Implemented interpolation parameter struct

--- src/ephemeris_interpolation.jl
+++ src/ephemeris_interpolation.jl
@@ -11,19 +11,23 @@ struct InterpolatedEphemeris
     function InterpolatedEphemeris(
         naif_id::String,
         ets,
-        rs,
+        rvs,
         rescale_epoch::Bool,
         tstar::Float64
     )
         if rescale_epoch
+            @warn "rescale_epoch == true is buggy"
             times_input = (ets .- ets[1]) / tstar
         else
             times_input = ets
         end
         splines = [
-            Spline1D(times_input, rs[1,:]),
-            Spline1D(times_input, rs[2,:]),
-            Spline1D(times_input, rs[3,:]),
+            Spline1D(times_input, rvs[1,:]),
+            Spline1D(times_input, rvs[2,:]),
+            Spline1D(times_input, rvs[3,:]),
+            Spline1D(times_input, rvs[4,:]),
+            Spline1D(times_input, rvs[5,:]),
+            Spline1D(times_input, rvs[6,:]),
         ]
         new(naif_id, (ets[1], ets[end]), splines, rescale_epoch, tstar)
     end
@@ -41,7 +45,7 @@ function Base.show(io::IO, ephem::InterpolatedEphemeris)
 end
 
 
-"""Interpolate ephemeris at a given epoch"""
+"""Interpolate ephemeris position at a given epoch"""
 function get_pos(ephem::InterpolatedEphemeris, et::Float64)
     if ephem.rescale_epoch
         et_eval = et * ephem.tstar + ephem.et_range[1]
@@ -53,6 +57,24 @@ function get_pos(ephem::InterpolatedEphemeris, et::Float64)
     return [Dierckx.evaluate(ephem.splines[1], et_eval),
             Dierckx.evaluate(ephem.splines[2], et_eval),
             Dierckx.evaluate(ephem.splines[3], et_eval)]
+end
+
+
+"""Interpolate ephemeris state at a given epoch"""
+function get_state(ephem::InterpolatedEphemeris, et::Float64)
+    if ephem.rescale_epoch
+        et_eval = et * ephem.tstar + ephem.et_range[1]
+        @assert ephem.et_range[1] <= et <= ephem.et_range[2]
+    else
+        et_eval = et
+        @assert ephem.et_range[1] <= et <= ephem.et_range[2]
+    end
+    return [Dierckx.evaluate(ephem.splines[1], et_eval),
+            Dierckx.evaluate(ephem.splines[2], et_eval),
+            Dierckx.evaluate(ephem.splines[3], et_eval),
+            Dierckx.evaluate(ephem.splines[4], et_eval),
+            Dierckx.evaluate(ephem.splines[5], et_eval),
+            Dierckx.evaluate(ephem.splines[6], et_eval)]
 end
 
 
@@ -70,30 +92,41 @@ mutable struct InterpolatedNbodyParams <: InterpolatedEphemerisParameters
         params::Nbody_params,
         N_step::Int = 10;
         rescale_epoch::Bool = false,
-        rescale_distance::Bool = true,
+        rescale_state::Bool = true,
     )
+        if rescale_epoch
+            @warn "rescale_epoch == true is buggy"
+        end
+
         # scaling
-        if rescale_distance
+        # scaling
+        if rescale_state
             lstar_rescale = params.lstar
+            vstar_rescale = params.vstar
         else
             lstar_rescale = 1.0
+            vstar_rescale = 1.0
         end
 
         # create splines
         ephem_dict = Dict{Int, InterpolatedEphemeris}()
         ets = range(et_range[1], et_range[2], N_step)
         for i = 2:length(params.mus_scaled)
-            rs_lt = spkpos.(
+            rvs_lt = spkezr.(
                 params.naif_ids[2],
                 ets,
                 params.naif_frame,
                 params.abcorr,
                 params.naif_ids[1]
             )
+            rvs = hcat([r_lt[1] for r_lt in rvs_lt]...)
+            rvs[1:3,:] /= lstar_rescale
+            rvs[4:6,:] /= vstar_rescale
+
             ephem_dict[i] = InterpolatedEphemeris(
                 params.naif_ids[2],
                 ets,
-                hcat([r_lt[1] for r_lt in rs_lt]...)/lstar_rescale,
+                rvs,
                 rescale_epoch,
                 params.tstar
             )
--- test/runtests.jl
+++ test/runtests.jl
@@ -13,10 +13,14 @@ furnsh(joinpath(spice_dir, "lsk", "naif0012.tls"))
 furnsh(joinpath(spice_dir, "spk", "de440.bsp"))
 furnsh(joinpath(spice_dir, "pck", "gm_de440.tpc"))
 
-@time @testset "Propagator tests" begin
+@time @testset "SPICE-based Propagators" begin
     include("test_jacobian.jl")
     include("test_Nbody_spice.jl")
     include("test_NbodySRP_spice.jl")
     include("test_Nbody_spice_lowapi.jl")
     include("test_stm.jl")
+end
+
+@time @testset "Ephemeris interpolation" begin
+    include("test_interpolate_ephem.jl")
 end
